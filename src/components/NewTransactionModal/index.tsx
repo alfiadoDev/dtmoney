920a37295b2f851ed4276bb39e5b6d8fddf81fab
Alfiado Constantino <alfiado.ngana@xit2s.com>
2022-10-27 13:28:19 +0200

    styling modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,12 @@
 import Modal from 'react-modal';
 
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+
+import { Container, TransactionTypeContainer } from './styles';
+
+
 interface ModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -10,8 +17,40 @@ export function NewTransactionModal({ isOpen, onRequestClose }: ModalProps) {
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
-      <h2>Cadastrar Transacao</h2>
+      <button type="button" onClick={onRequestClose} className="react-modal-close">
+        <img src={closeImg} alt="Fechar Modal" />
+      </button>
+
+      <Container>
+        <h2>Cadastrar Transacao</h2>
+
+        <input type="text" placeholder='Titulo' />
+
+        <input type="number" placeholder='Valor' />
+
+        <TransactionTypeContainer>
+          <button
+            type='button'
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button
+            type='button'
+          >
+            <img src={outcomeImg} alt="Saida" />
+            <span>Saida</span>
+          </button>
+        </TransactionTypeContainer>
+
+        <input type="text" placeholder='Categoria' />
+
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 }
